977665f7ce3b9981de582bdeba7f4dd8b4b5d0e9
Anthony Tu <antu3199@gmail.com>
2019-06-20 17:28:41 -0400

    Camera2 (#7)
    * Background & camera
    
    * Merged camera

---
+++ Element/Assets/_Scripts/Player/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
