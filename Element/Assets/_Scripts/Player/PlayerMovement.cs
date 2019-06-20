ef5115d2d8bb864992f2439d85574311902f4c4f
Anthony Tu <antu3199@gmail.com>
2019-06-20 17:24:40 -0400

    Background & camera

---
+++ Element/Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+    private Vector3 prevMousePos = Vector3.zero;
+    public float maxSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        prevMousePos = Input.mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButton(0)) {
+			Vector3 deltaMouse = Input.mousePosition - prevMousePos;
+      deltaMouse = new Vector3(Mathf.Clamp(deltaMouse.x, -this.maxSpeed, this.maxSpeed), Mathf.Clamp(deltaMouse.y, -this.maxSpeed, this.maxSpeed), deltaMouse.z);
+			this.transform.position += deltaMouse * movementSpeed * Time.deltaTime;
+		}
+
+		this.prevMousePos = Input.mousePosition;
+    }
+}
