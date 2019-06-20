73474fd303495ef12fb913cc9c1952028806bcad
Anthony Tu <antu3199@gmail.com>
2019-06-20 17:51:13 -0400

    Particles

--- Element/Assets/_Scripts/Particle/ParticleSpawn.cs
+++ Element/Assets/_Scripts/Particle/ParticleSpawn.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleSpawn : MonoBehaviour {
-
-	public GameObject particle;
+  
+  public Transform player;
+	public PooledObject particle;
 	float randX, randY;
 	Vector2 whereToSpawn;
 	public float spawnRate = 2f;
 	float nextSpawn = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		if (Time.time > nextSpawn){
 			nextSpawn = Time.time + spawnRate;
 			randX = Random.Range(-9.0f,9.0f);
 			randY = Random.Range(-4.3f,4.3f);
-			whereToSpawn = new Vector2 (randX, randY);
-			Instantiate (particle, whereToSpawn, Quaternion.identity);
+			whereToSpawn = player.position + new Vector3 (randX, randY, 1);
+
+      PooledObject particle = this.particle.GetPooledInstance<PooledObject>(whereToSpawn);
 		}
 	}
 }
