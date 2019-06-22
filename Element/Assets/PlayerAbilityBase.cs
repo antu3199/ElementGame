22b6659e524cf00f0b638b3968b1179bc5933af5
Anthony Tu's laptop <antu3199@gmail.com>
2019-06-22 17:02:14 -0400

    Abilities

---
+++ Element/Assets/PlayerAbilityBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilityBase : MonoBehaviour {
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			this.useAbility();
+		}
+	}
+	public virtual void useAbility() {}
+}
---
+++ Element/Assets/WaterAbility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAbility : PlayerAbilityBase {
+    
+	public float healAmount;
+	public override void useAbility() {
+		GameStateManager.Instance.player.playerHealth.updateHealth(healAmount);
+	}
+}
