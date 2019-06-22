674ca7b5aa0937c8f70fec9846ead5fc399c4d46
DhivoGnani <dgnanara@gmail.com>
2019-06-22 16:10:49 -0400

    Fix double tap

--- Element/Assets/DoubleTap.cs
+++ Element/Assets/DoubleTap.cs
@@ -4,42 +4,15 @@ using UnityEngine;
 
 public class DoubleTap : MonoBehaviour
 {
-
-    int TapCount;
-    float NewTime;
-
     void Start()
     {
-        TapCount = 0;
     }
 
     void Update()
     {
-        if (Input.touchCount == 1)
+        if (Input.touchCount == 2)
         {
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Ended)
-            {
-                TapCount += 1;
-            }
-
-            if (TapCount == 1)
-            {
-
-                NewTime = Time.time + 1f;
-            }
-            else if (TapCount == 2 && Time.time <= NewTime)
-            { 
-                print("double tap");
-
-                TapCount = 0;
-            }
-
-        }
-        if (Time.time > NewTime)
-        {
-            TapCount = 0;
+            print("Use power!");
         }
     }
 }
