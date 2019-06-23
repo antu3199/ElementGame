ed9ffed0a63dfa68aebd3351fa85acd15e6ae933
Anthony Tu <antu3199@gmail.com>
2019-06-22 20:27:40 -0400

    Ability activation (#16)
    * start ability activation
    
    * ability activation

--- Element/Assets/_Scripts/Player/ParticleTransformer.cs
+++ Element/Assets/_Scripts/Player/ParticleTransformer.cs
@@ -5,59 +5,83 @@ using UnityEngine.UI;
 
 
 
-public class ParticleTransformer : MonoBehaviour {
+public class ParticleTransformer : MonoBehaviour
+{
 
-    // TODO: Add prefab for each particle instead...
-	public Image particleTransformInto;
-    public CanvasGroup fadeInGroup;
-	public float fullAlpha;
-	public float transitionSpeed;
-	public float displayTime;
-	private bool isTransforming = false;
+  // TODO: Add prefab for each particle instead...
+  public CanvasGroup fadeInGroup;
+  public float fullAlpha;
+  public float transitionSpeed;
+  public float displayTime;
+  private bool isTransforming = false;
 
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.A)) {
-			this.TransformParticle();
-		}
-	}
+  // Variables for screen info:
+  public RectTransform particleImageContainer;
+  private GameObject curTransformObject;
+  
+  public Text commonNameText;
+  public Text formulaText;
+  public Text descriptionText;
 
-	public void TransformParticle() {
+  void Start()
+  {
+  }
 
-		if (this.isTransforming) return;
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.A))
+    {
+      this.TransformParticle();
+    }
+  }
 
-		PlayerAbilityController abilityController = GameStateManager.Instance.player.playerAbility;
-        
-		int randIndex = abilityController.GetRandAbilityIndex();
-		// Set particle info here...
-		PlayerAbilityBase info = GameStateManager.Instance.player.playerAbility.TransformParticle();
-		Debug.Log(info.commonName);
+  public void TransformParticle()
+  {
 
-		StartCoroutine(this.transformCor());
-	}
+    if (this.isTransforming) return;
 
-	private IEnumerator transformCor() {
-		float t = 0;
-		this.fadeInGroup.gameObject.SetActive(true);
-		this.isTransforming = true;
-		while (t <= 1) {
-			t += transitionSpeed * Time.deltaTime; 
-	    	this.fadeInGroup.alpha = Mathf.Lerp(0, this.fullAlpha, t);
-			yield return null;
-		}
+    PlayerAbilityController abilityController = GameStateManager.Instance.player.playerAbility;
+    // Set particle info here...
+    PlayerAbilityBase info = GameStateManager.Instance.player.playerAbility.TransformParticle();
+    GameObject particleImage =  Instantiate(info.canvasVisualsPrefab, this.particleImageContainer.transform);
+    particleImage.transform.SetParent(this.particleImageContainer.transform);
+    this.curTransformObject = particleImage;
 
-		yield return new WaitForSeconds(this.displayTime);
+    this.commonNameText.text = info.commonName;
+    this.formulaText.text = info.chemicalName;
+    this.descriptionText.text = info.description;
+    
+    Debug.Log(info.commonName);
 
-		while (t > 0) {
-			t -= transitionSpeed * Time.deltaTime; 
-	    	this.fadeInGroup.alpha = Mathf.Lerp(0, this.fullAlpha, t);
-			yield return null;
-		}
-		yield return null;
-		this.fadeInGroup.gameObject.SetActive(false);
-		this.isTransforming = false;
-	} 
+    StartCoroutine(this.transformCor());
+  }
+
+  private IEnumerator transformCor()
+  {
+    float t = 0;
+    this.fadeInGroup.gameObject.SetActive(true);
+    this.isTransforming = true;
+    while (t <= 1)
+    {
+      t += transitionSpeed * Time.deltaTime;
+      this.fadeInGroup.alpha = Mathf.Lerp(0, this.fullAlpha, t);
+      yield return null;
+    }
+
+    GameStateManager.Instance.UpdatePoints(0);
+
+    yield return new WaitForSeconds(this.displayTime);
+
+    while (t > 0)
+    {
+      t -= transitionSpeed * Time.deltaTime;
+      this.fadeInGroup.alpha = Mathf.Lerp(0, this.fullAlpha, t);
+      yield return null;
+    }
+    yield return null;
+    Destroy(this.curTransformObject);
+    this.fadeInGroup.gameObject.SetActive(false);
+    this.isTransforming = false;
+  }
 }
--- Element/Assets/_Scripts/Player/PlayerCollision.cs
+++ Element/Assets/_Scripts/Player/PlayerCollision.cs
@@ -13,17 +13,13 @@ public class PlayerCollision : MonoBehaviour {
       // Do particle point thing
       PooledObject particle = other.gameObject.GetComponent<PooledObject>();
       particle.ReturnToPool();
-      GameStateManager.Instance.player.playerPoints.UpdatePoint(1);
-      print("Points: " + GameStateManager.Instance.player.playerPoints.points);
-      GameStateManager.Instance.ui.slider.value = System.Math.Min(1f,
-          GameStateManager.Instance.player.playerPoints.points / GameStateManager.Instance.player.playerPoints.maxPoints);
+      GameStateManager.Instance.UpdatePoints(1);
+   
     } else if (other.tag == "EnemyBullet") {
       Bullet bulletScript = other.transform.parent.GetComponent<Bullet>();
       bulletScript.DestroyBullet();
-      GameStateManager.Instance.player.playerHealth.updateHealth(-1);
-      print("Player's Health: " + GameStateManager.Instance.player.playerHealth.health);
-      GameStateManager.Instance.ui.healthSlider.value = System.Math.Max(0f,
-          GameStateManager.Instance.player.playerHealth.health / GameStateManager.Instance.player.playerHealth.maxHealth);
+      GameStateManager.Instance.updateHealth(-1);
+
     }
   }
 }
