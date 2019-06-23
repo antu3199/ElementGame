356c71c0c976b68aedc6dcde906616aa5dadc28c
J K <kskeum@uwaterloo.ca>
2019-06-23 14:33:02 -0400

    added alcohol

---
+++ Element/Assets/_Scripts/Abilities/AlcoholAbility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlcoholAbility : PlayerAbilityBase {
+    public override PARTICLE_TYPES type { get { return PARTICLE_TYPES.ALCOHOL; }  }
+	public override string commonName { get { return "Alcohol"; }  }
+	public override string chemicalName { get { return "OH"; }  }
+  public override string description { get { 
+    return "Description: Alcohol is a family of compounds that have OH attached to the carbon molecule.\n\n Gameplay: Tap the screen with two fingers to destory the surrounding bullets (on a cooldown)";
+  }}
+
+  public override bool useableAbility { get { return true; } }
+
+    public Transform circleObject;
+    public float increaseAmount;
+    public float timeDuration;
+
+    public override void useAbility() {
+        /// Expand the circle; 
+        StartCoroutine(expandCircle());
+    }
+
+    IEnumerator expandCircle() {
+        float time = 0;
+        this.circleObject.localScale = new Vector3(0, 0, this.circleObject.localScale.z);
+        this.circleObject.gameObject.SetActive(true);
+
+        while (time < timeDuration) {
+            time += Time.deltaTime;
+            this.circleObject.localScale = new Vector3(this.circleObject.localScale.x + increaseAmount, this.circleObject.localScale.y + increaseAmount, this.circleObject.localScale.z);
+            yield return null;
+        }
+        this.circleObject.gameObject.SetActive(false);
+    }
+}
+
