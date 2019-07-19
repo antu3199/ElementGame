21b5d9c27c73afba86c423d25d803f185269549e
keumie <12888132+keumie@users.noreply.github.com>
2019-07-19 11:00:53 -0400

    More Abilities (#32)
    * Added 3 Abilities
    
    Added Cyanide, Pheno2qi3jefwaklmsn, and BakingSoda
    
    baking soda is op af LOL
    
    * updated prefab

---
+++ Element/Assets/_Scripts/Abilities/CyanideAbility.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Ability the "heals" the player
+public class CyanideAbility : PlayerAbilityBase {
+    public override PARTICLE_TYPES type { get { return PARTICLE_TYPES.CYANIDE; } }
+	public override string commonName { get { return "Cyanide"; }  }
+	public override string chemicalName { get { return "CN"; }  }
+  public override string description { get { 
+    return "Description: Cyanide is a potentially very deadly chemical found in various forms in nature.\n\n Gameplay: Continuously lose health";
+  }}
+
+    public override bool useableAbility { get { return false; } }
+
+    float curTime = 0;
+    public float drainAmount = -1;
+    // Update is called once per frame
+    public new void Update()
+    {
+        base.Update();
+        // Increase score over time
+        curTime += Time.deltaTime;
+        if (curTime >= 5)
+        {
+            GameStateManager.Instance.game.updateHealth(drainAmount);
+            curTime = 0;
+        }
+    }
+}
---
+++ Element/Assets/_Scripts/Abilities/PhenolphthaleinAbility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Create "armor" that blocks a few bullets
+public class PhenolphthaleinAbility : PlayerAbilityBase
+{
+    public override PARTICLE_TYPES type { get { return PARTICLE_TYPES.PHENO; } }
+    public override string commonName { get { return "Phenolphthalein"; } }
+    public override string chemicalName { get { return "((C)20(H)14(O)4)n"; } }
+    public override string description
+    {
+        get
+        {
+            return "Description: Phenolphthalein is an organic compound that is often used in pH indicators. Turns colourless in acidic solutions.\n\n Gameplay: Tap to turn invisible to dodge bullets for few seconds";
+        }
+    }
+
+    public override bool useableAbility { get { return true; } }
+
+    public Transform playerMolecule;
+    public float effectDuration;
+
+    public override void useAbility()
+    {
+        /// Expand the circle; 
+        StartCoroutine(this.disappearEffectCoroutine());
+    }
+
+    private IEnumerator disappearEffectCoroutine()
+    {
+        this.playerMolecule.gameObject.SetActive(false);
+        GameStateManager.Instance.game.player.playerCollision.hasArmor = true;
+        yield return new WaitForSeconds(this.effectDuration);
+        this.playerMolecule.gameObject.SetActive(true);
+        GameStateManager.Instance.game.player.playerCollision.hasArmor = false;
+    }
+}
